384328e8b178822f3c2c49db2e8c6a1a78d17c3c
Timothy <timothyolofson@gmail.com>
2025-01-17 02:01:21 -0800

    i need help, im suck and can't find the bug. it stopped working at the .createElement('div') and never console logged any <divs> in the console. I kept going any way. I'm not sure whats causing it to not give me any errors either.

--- movies.js
+++ movies.js
@@ -8,19 +8,18 @@ const movieSearchBox = document.getElementById('search__movie--text-box');
 const searchList = document.getElementById('search__movie--list');
 const resultGrid = document.getElementById('result__grid');
 
-// load movies from API
+// Load Movies from API //
 
 async function loadMovies(searchTerm) {
 
-  const URL = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=dfa6e870`;
+  const URL = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=32abe755`;
   const res = await fetch(`${URL}`);
   const  data = await res.json();
 
-  if (data.response == 'True') {
-
-    displayMovieList(data.Search);
-  }
+  if (data.Response == 'True') displayMovieList(data.Search);
 }
+
+// Search Box //
 
 function findMovies() {
 
@@ -28,16 +27,51 @@ function findMovies() {
 
   if (searchTerm.length > 0) {
 
-    searchList.classList.remove('search__movie--hide-list') {
+    searchList.classList.remove('hide__search--list'); 
+    loadMovies(searchTerm);
 
-    } else {
-
-      searchList.classList.add('search__movie--hide-list')
-    }
+  } else {
+      searchList.classList.add('hide__search--list');
   }
 }
 
+// Movie List Dropdown //
+
 function displayMovieList(movies) {
 
+  searchList.innerHTML = '';
 
+  for ( let idx = 0; idx < movies.length; idx++ ) {
+
+    let movieListItem = document.createElement('div');
+
+    movieListItem.dataset.id = movies[idx].imdbID;
+    movieListItem.classList.add('search__movie--list-item');
+
+    if (movies[idx].Poster != 'N/A')
+
+      moviePoster = movies[idx].Poster;
+
+    else 
+
+      moviePoster = 'image_not_found.png';
+    
+    movieListItem.innerHTML = `
+    
+      <div class="search__movie--item-thumbnail">
+
+        <img src="${moviePoster}">
+
+      </div>
+
+      <div class="search__movie--item-info">
+
+        <h3>${movies[idx].Title}</h3>
+        <p>${movies[idx].Year}</p>
+
+      </div>
+
+    `;
+    searchList.appendChild(movieListItem);
+  }
 }
